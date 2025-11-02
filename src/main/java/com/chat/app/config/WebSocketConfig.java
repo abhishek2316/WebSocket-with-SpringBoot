a1de2a2eb927eb153d71edcce3c50bc303628810
abhishek2316 <kumarabhi1623@gmail.com>
2025-11-03 01:24:05 +0530

    Backend is listening correctly form the frontend

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 //        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
-        registry.enableSimpleBroker("/topics");
+        registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
